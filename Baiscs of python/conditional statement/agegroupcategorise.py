79ac49324af6988ad6462967e373445923904d2f
polranirav <polranirav@gmail.com>
2025-07-07 19:26:13 -0400

    j

---
+++ Baiscs of python/conditional statement/agegroupcategorise.py
@@ -0,0 +1,10 @@
+age = input("enter your age:")
+age = int(age)
+if age < 13:
+    print("Child")
+elif age < 20:
+    print("Teenager")
+elif age < 60:
+    print("Adult")
+else:
+    print("Senior")
